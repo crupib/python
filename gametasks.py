cb99124eaf742adc402b1795db909a265d9e8cc8
crupib <crupib@gmail.com>
2018-03-18 13:19:02 -0400

    add more to game

--- gametasks.py
+++ gametasks.py
@@ -4,16 +4,17 @@ counts = []
 def printInstructions(instruction):
     print(instruction)
 
-
 def getUserScore(userName):
     filename = open("userScores.txt", "r")
     for line in filename:
         counts.append(line.split(","))
-    for ustring in counts:
-        if userName == ustring[0]:
-            print("user in counts")
-
-
+    for name_score in counts:
+        if userName == name_score[0]:
+            return(name_score[1])
+        else:
+            continue
+    return(-1)
+# Main  #
 userName = ""
-getUserScore("Darren")
-print(counts)
+score = getUserScore("xenny")
+print(score)
