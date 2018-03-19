6bd25ba74873bc51acf340ba384b836b47b7ce48
BillCrupi <bill.crupi@timeinc.com>
2018-03-19 15:18:52 -0400

    gametasks

--- gametasks.py
+++ gametasks.py
@@ -1,20 +1,63 @@
+import os
 counts = []
 
 
 def printInstructions(instruction):
     print(instruction)
 
+
+def updateUserScore(newUser, userName, score):
+    if newUser == True:
+        try:
+            filename = open("userScores.txt", "a")
+            filename.write("%s, %s\n" % (userName, score))
+            filename.close()
+            return(0)
+        except IOError:
+            print("no userScores.txt file")
+            filename = open("userScores.txt", "w")
+            filename.write(userName, score)
+            filename.close()
+            return(0)
+
+    else:
+        tmpname = open("userScores.tmp", "w")
+        filename = open("userScores.txt", "r")
+        for line in filename:
+            counts.append(line.split(","))
+        for name_score in counts:
+            if userName == name_score[0]:
+                tmpname.write("%s, %s\n" % (userName, score))
+            else:
+                tmpname.write("%s, %s" % (name_score[0], name_score[1]))
+        tmpname.close()
+        filename.close()
+        os.rename("userScores.txt","_userScores.txt")
+        os.rename("userScores.tmp","userScores.txt")
+
+
 def getUserScore(userName):
-    filename = open("userScores.txt", "r")
+    try:
+        filename = open("userScores.txt", "r")
+    except IOError:
+        filename = open("userScores.txt", "w")
+        filename.close()
+        return (-1)
     for line in filename:
         counts.append(line.split(","))
     for name_score in counts:
         if userName == name_score[0]:
+            filename.close()
             return(name_score[1])
         else:
             continue
+    filename.close()
     return(-1)
+
+
 # Main  #
 userName = ""
-score = getUserScore("xenny")
-print(score)
+#score = getUserScore("Benny")
+# print(score)
+updateU = updateUserScore(False, "Darren", "259")
+#print('update user {0} \n'.format(updateU))
