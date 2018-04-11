1787172a49023703dc0abc949a2644def455eac2
crupib <crupib@gmail.com>
2018-04-10 21:26:07 -0400

    Test

--- gametasks.py
+++ gametasks.py
@@ -1,55 +1,40 @@
-import os
-counts = []
-
-
 def printInstructions(instruction):
     print(instruction)
 
 
 def updateUserScore(newUser, userName, score):
+    from os import remove, rename
     if newUser == True:
-        try:
-            filename = open("userScores.txt", "a")
-            filename.write("%s, %s\n" % (userName, score))
-            filename.close()
-            return(0)
-        except IOError:
-            print("no userScores.txt file")
-            filename = open("userScores.txt", "w")
-            filename.write(userName, score)
-            filename.close()
-            return(0)
-
+        input = open("userScores.txt", "a")
+        input.write(userName + ', ' +  score + '\n')
+        input.close()
     else:
-        tmpname = open("userScores.tmp", "w")
-        filename = open("userScores.txt", "r")
-        for line in filename:
-            counts.append(line.split(","))
-        for name_score in counts:
-            if userName == name_score[0]:
-                tmpname.write("%s, %s\n" % (userName, score))
+        temp = open("userScores.tmp", "w")
+        input = open("userScores.txt", "r")
+        for line in input:
+            content = (line.split(","))
+            if content[0] == userName:
+                input.write(userName + ', ' +  score + '\n')
             else:
-                tmpname.write("%s, %s" % (name_score[0], name_score[1]))
-        tmpname.close()
-        filename.close()
-        os.rename("userScores.txt","_userScores.txt")
-        os.rename("userScores.tmp","userScores.txt")
+                temp.write(line)
+        temp.close()
+        input.close()
+        remove("userScores.txt")
+        rename("userScores.tmp", "userScores.txt")
 
 
 def getUserScore(userName):
     try:
-        filename = open("userScores.txt", "r")
+        input = open("userScores.txt", "r")
+        for line in input:
+            content = line.split(", ")
+            if content[0] == userName:
+                input.close()
+                return content[1]
+        input.close()
+        return '-1'
     except IOError:
-        filename = open("userScores.txt", "w")
-        filename.close()
-        return (-1)
-    for line in filename:
-        counts.append(line.split(","))
-    for name_score in counts:
-        if userName == name_score[0]:
-            filename.close()
-            return(name_score[1])
-        else:
-            continue
-    filename.close()
-    return(-1)
+        print("File not found. A new file will be created.")
+        input = open('userScores.txt', 'w')
+        input.close()
+        return '-1'
