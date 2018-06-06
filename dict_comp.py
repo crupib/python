ec3ce76aceb833ef2727886b84effcbee15ca3b1
BillCrupi <bill.crupi@timeinc.com>
2018-06-06 10:09:29 -0400

    simple packages

---
+++ dict_comp.py
@@ -0,0 +1,5 @@
+word = 'letters'
+letter_counts = {letter: word.count(letter) for letter in word}
+print(letter_counts)
+letter_counts2 = {letter: word.count(letter) for letter in set(word)}
+print(letter_counts2)
