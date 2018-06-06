443f90edcbb2b08d527490c5017c262dbe0c4d24
BillCrupi <bill.crupi@timeinc.com>
2018-06-06 09:19:19 -0400

    Simple Packages

---
+++ range.py
@@ -0,0 +1,7 @@
+for x in range(0,3):
+	print(x)
+print(list(range(0,3)))
+for x in range(2,-1,-1):
+	print(x)
+print(list(range(2,-1,-1)))
+print(list(range(0,11,2)))
---
+++ zipdict.py
@@ -0,0 +1,4 @@
+english = 'Monday', 'Tuesday', 'Wednesday'
+french  = 'Lundi', 'Mardi', 'Mercredi'
+print(list(zip(english,french)))
+print(dict(zip(english,french)))
