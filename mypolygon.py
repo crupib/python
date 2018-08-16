de3468ca89ffe4a94251e93e8c36ff43713f6a0b
crupib <crupib@gmail.com>
2018-08-16 11:52:01 -0400

    Think like CS Python

---
+++ mypolygon.py
@@ -0,0 +1,22 @@
+import turtle
+bob = turtle.Turtle()
+print(bob)
+#print square
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+
+bob.rt(90)
+bob.fd(100)
+bob.rt(90)
+bob.fd(100)
+bob.rt(90)
+bob.fd(100)
+for i in range(4):
+	print('Hello!')
+
+turtle.mainloop()
