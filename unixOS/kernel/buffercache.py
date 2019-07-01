7e626654891ae385ca7fa8f57365521a5fd7997f
root <williamcrupi@Williams-MacBook-Pro.local>
2019-07-01 09:17:21 -0400

    NTS updates

--- unixOS/kernel/buffercache.py
+++ unixOS/kernel/buffercache.py
@@ -1,1 +1,14 @@
 import double_linklist
+class buffer_head:
+    def __init__(self, devnum, blocknum,status, dataarea):
+       self.devnum = None
+       self.blocknum  = None
+       self.status = None
+       self.dataarea = dataarea
+       self.nexthash = None
+       self.prevhash = None
+       self.nextfree = None
+       self.prevfree = None
+       self.next = None 
+       self.prev = None
+
--- unixOS/kernel/double_linklist.py
+++ unixOS/kernel/double_linklist.py
@@ -3,15 +3,6 @@ class Node:
        self.data = data
        self.next = None
        self.prev = None
-class buffer_head:
-    def __init__(self, devnum, blocknum,status, dataarea):
-       self.dataarea = dataarea
-       self.nexthash
-       self.prevhash
-       self.nextfree
-       self.prevfree
-       self.next = None
-       self.prev = None
  
  
 class DoublyLinkedList:
@@ -76,53 +67,54 @@ class DoublyLinkedList:
             print(current.data, end = ' ')
             current = current.next
  
+''' 
+a_dllist = DoublyLinkedList()
  
-#a_dllist = DoublyLinkedList()
+print('Menu')
+print('insert <data> after <index>')
+print('insert <data> before <index>')
+print('insert <data> at beg')
+print('insert <data> at end')
+print('remove <index>') 
+print('quit')
  
-#print('Menu')
-#print('insert <data> after <index>')
-#print('insert <data> before <index>')
-#print('insert <data> at beg')
-#print('insert <data> at end')
-#print('remove <index>') 
-#print('quit')
+while True:
+    print('The list: ', end = '')
+    a_dllist.display()
+    print()
+    do = input('What would you like to do? ').split()
  
-#while True:
-#    print('The list: ', end = '')
-#    a_dllist.display()
-#    print()
-#    do = input('What would you like to do? ').split()
+    operation = do[0].strip().lower()
  
-#    operation = do[0].strip().lower()
+    if operation == 'insert':
+        data = int(do[1])
+        position = do[3].strip().lower()
+        new_node = Node(data)
+        suboperation = do[2].strip().lower() 
+        if suboperation == 'at':
+            if position == 'beg':
+                a_dllist.insert_at_beg(new_node)
+            elif position == 'end':
+                a_dllist.insert_at_end(new_node)
+        else:
+            index = int(position)
+            ref_node = a_dllist.get_node(index)
+            if ref_node is None:
+                print('No such index.')
+                continue
+            if suboperation == 'after':
+                a_dllist.insert_after(ref_node, new_node)
+            elif suboperation == 'before':
+                a_dllist.insert_before(ref_node, new_node)
  
-#    if operation == 'insert':
-#        data = int(do[1])
-#        position = do[3].strip().lower()
-#        new_node = Node(data)
-#        suboperation = do[2].strip().lower() 
-#        if suboperation == 'at':
-#            if position == 'beg':
-#                a_dllist.insert_at_beg(new_node)
-#            elif position == 'end':
-#                a_dllist.insert_at_end(new_node)
-#        else:
-#            index = int(position)
-#            ref_node = a_dllist.get_node(index)
-#            if ref_node is None:
-#                print('No such index.')
-#                continue
-#            if suboperation == 'after':
-#                a_dllist.insert_after(ref_node, new_node)
-#            elif suboperation == 'before':
-#                a_dllist.insert_before(ref_node, new_node)
-# 
-#    elif operation == 'remove':
-#        index = int(do[1])
-#        node = a_dllist.get_node(index)
-#        if node is None:
-#            print('No such index.')
-#            continue
-#        a_dllist.remove(node)
+    elif operation == 'remove':
+        index = int(do[1])
+        node = a_dllist.get_node(index)
+        if node is None:
+            print('No such index.')
+            continue
+        a_dllist.remove(node)
  
-#    elif operation == 'quit':
-#        break
+    elif operation == 'quit':
+        break
+'''
