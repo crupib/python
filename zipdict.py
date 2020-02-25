bc60754a4b781bd337611ccc73248c09e8231180
crupib <crupib@gmail.com>
2020-02-25 18:53:52 -0500

    sync

--- zipdict.py
+++ zipdict.py
@@ -1,4 +1,9 @@
 """zip list."""
+def inc(x):
+  return x + 1
+def test_answer():
+   assert inc(3) == 5
+
 ENGLISH = 'Monday', 'Tuesday', 'Wednesday'
 FRENCH = 'Lundi', 'Mardi', 'Mercredi'
 print(list(zip(ENGLISH, FRENCH)))
