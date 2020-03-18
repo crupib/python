93a9dbfb4607d7215908dee6a69410f204177619
crupib <crupib@gmail.com>
2020-03-18 19:07:35 -0400

    Learning

---
+++ learning.py
@@ -0,0 +1,4 @@
+myName = input("Please enter you name: ")
+myAge = input("Hi %s, what about your age: "%(myName))
+myAge = input("Hi {}, what about your age: ".format(myName))
+print("Hello World, my name is",myName,"and I am", myAge, "years old.")
