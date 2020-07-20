854017694529c0b790012ad0accb26c05d24b348
crupib <crupib@gmail.com>
2020-07-20 14:09:06 -0400

    polygon

--- mypolygon.py
+++ mypolygon.py
@@ -13,18 +13,18 @@ def arc(t, r, angle):
    n = int(arc_length / 3) + 1
    step_length = arc_length / n
    step_angle = angle / n
-   
    for i in range(n):
        t.fd(step_length)
        t.lt(step_angle)
 
 def polyline(t, n, length, angle):
+   """Draws n line segments with the given length and angle (in degrees) between them. t is turtle.
+   """
    for i in range(n):
       t.fd(length)
       t.lt(angle)      
 bob = turtle.Turtle()
 #polygon(bob,n=11,length=70)
 #circle(bob,100)
-arc(bob, 50, 180)
+arc(bob, 50, 275)
 turtle.mainloop()
-	
