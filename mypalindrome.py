2cc918279e3c85ebed2fc4e412ccb7e03bc53e91
Bill Crupi <bill.crupi@ntsdev.net>
2020-09-04 15:03:18 -0400

    palindrome

--- mypalindrome.py
+++ mypalindrome.py
@@ -2,4 +2,4 @@ def palindrome(word):
     lowercase = word.lower()
     alpha_only = ''.join([i for i in lowercase if i.isalpha()])
     return alpha_only == alpha_only[::-1]
-print(palindrome('A nut for a jar of tuna'))
+print(palindrome('A nut, for a jar of tuna!'))
