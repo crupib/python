ac15b19d7104ffc9f6399d64a23a29445faba5a9
Bill Crupi <bill.crupi@ntsdev.net>
2020-11-18 14:21:05 -0500

    argparse

---
+++ argp2.py
@@ -0,0 +1,9 @@
+import argparse
+parser=argparse.ArgumentParser(description="add numbers")
+parser.add_argument("first", type=int)
+parser.add_argument("second", type=int)
+args = parser.parse_args()
+x = args.first
+y = args.second
+z = x+y
+print ('addition of {} and {} = {}'.format(x,y,z))
