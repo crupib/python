0fe5c76ed19e5126bc00ab9fad0d64f7279f421e
bill <crupib@gmail.com>
2021-03-30 10:14:19 -0400

    opencv

--- __init__.py
+++ __init__.py
@@ -1,7 +1,3 @@
-from . import plots
-from . import tools
-from .plots import cm3, cm2
-from .tools import discrete_scatter
-from .plot_helpers import ReBl
+import os
 
-__all__ = ['tools', 'plots', 'cm3', 'cm2', 'discrete_scatter', 'ReBl']
+haarcascades = os.path.join(os.path.dirname(__file__), "")
