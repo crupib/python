c956b3b86b32f379f37153b748ebfeec52d69b1e
bill <crupib@gmail.com>
2021-07-09 16:12:08 -0400

    more dnac crap

--- dnac_get_vlan.py
+++ dnac_get_vlan.py
@@ -3,9 +3,10 @@ from requests.auth import HTTPBasicAuth
 import json
 import urllib3
 urllib3.disable_warnings()
-# API URL TO GET A LIST OF NETWORK DEVICES
+#Public username and password from Cisco website.
 USERNAME="devnetuser"
 PASSWORD="Cisco123!"
+# API URL TO GET A LIST OF NETWORK DEVICES
 url = "https://sandboxdnac2.cisco.com/dna/intent/api/v1/network-device"
 def get_auth_token():
     url = 'https://sandboxdnac2.cisco.com/dna/system/api/v1/auth/token'
@@ -26,7 +27,19 @@ raw_output = json.loads(response.text)
 
 # raw_output IS A DICTIONARY. TAKE THE VALUE OF response AND ASSIGN TO devices
 devices = raw_output["response"]
- 
+id = [] 
 # ITERATE THROUGH THE LIST TO PRINT OUT HOSTNAMES OF DEVICES
 for device in devices:
-    print("ID: {}".format(device["id"]),"Hostname: {}".format(device["hostname"]),"MACADDRESS: {}".format(device["macAddress"]))
+#    print("ID: {}".format(device["id"]),"Hostname: {}".format(device["hostname"]),"MACADDRESS: {}".format(device["macAddress"]))
+    id.append(device["id"])
+for deviceid in id:
+   payload = {}
+   url=f"https://sandboxdnac2.cisco.com/dna/intent/api/v1/network-device/{deviceid}/vlan"
+   response = requests.request("GET", url, verify=False, headers=headers, data=payload)
+   raw_output = json.loads(response.text)  
+   print(raw_output)
+   vlans = raw_output["response"]
+   #print(vlans)
+   for vlan in raw_output:
+         if response == 'no
+         print("vlan #: {}".format(vlan["vlanNumber"]))
