779625514cc165d6269193472b1a649a1b8083e3
crupib <crupib@gmail.com>
2021-07-11 15:50:01 -0400

    shell working

--- Ex_Files_Programming_Foundations_Algorithms/Exercise Files/data/immig.py
+++ Ex_Files_Programming_Foundations_Algorithms/Exercise Files/data/immig.py
@@ -1,30 +1,26 @@
-import mybubsort
+import shellsort
+#import mybubsort
 
 def LinearSearch(list, item):
   index = 0
-  found = False
-  while index < len(list) and  found is False:
+  found = []
+  while index < len(list):
     if list[index][4] == item:
-      found = True
+      found.append(list[index])
+      index = index + 1
     else:
       index = index + 1
   return found
 
-
+found = []
 ifile=open("hist.txt", "r")
 lines=ifile.readlines()
 data=[tuple(line.strip().split()) for line in lines]
-print("*************************************************************************")
-print()
-i = 0
-while i < len(data):
-  print(data[i])
-  i = i + 1
-i = 0
-print("*************************************************************************")
-print()
-mybubsort.bubbleSort(data)
-while i < len(data):
-  print(data[i])
-  i = i + 1
-print(LinearSearch(data,"2000-09-19"))
+shellsort.shellSort(data)
+print("******************************************************")
+for item in data:
+   print(item) 
+j=LinearSearch(data,"2000-09-19")
+print("******************************************************")
+for item in j:
+   print(item)
