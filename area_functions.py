dd0c07f6126ab732f5b917e46db61e50c7631eaf
bill <crupib@gmail.com>
2021-07-19 10:10:54 -0400

    Flash cards

---
+++ area_functions.py
@@ -0,0 +1,10 @@
+"""A set of functions for calculating areas."""
+import math		
+def area_rect(w, h):
+   """Calculate the area of a rectangle. """
+   area = w * h
+   return area
+def area_circle(r):
+    """Calculate the area of a circle."""
+    area_circle = math.pi * r * r 
+    return area_circle
---
+++ func.py
@@ -0,0 +1,6 @@
+from area_functions import area_rect, area_circle
+
+my_area = area_rect(5,4)
+print(my_area)
+my_area = area_circle(2)
+print(my_area)
