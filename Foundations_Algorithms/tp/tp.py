6cb1eccd4fa35bb9b9feddef989cd44f7c4355ec
bill <crupib@gmail.com>
2021-07-26 19:21:48 -0400

    brute

---
+++ Foundations_Algorithms/tp/tp.py
@@ -0,0 +1,56 @@
+import random
+from itertools import permutations
+import matplotlib.pyplot as plt
+from time import perf_counter
+from collections import Counter
+
+alltours = permutations
+aCity = complex
+
+def distance_tour(aTour):
+    return sum(distance_points(aTour[i - 1], aTour[i])
+               for i in range(len(aTour)))
+
+def distance_points(first, second): return abs(first - second)
+
+def generate_cities (number_of_cities):
+    seed = 111; width = 500; height=300
+    random.seed((number_of_cities))
+    return frozenset(aCity(random.randint(1,width), random.randint(1,height))
+                     for c in range(number_of_cities))
+
+def brute_force(cities):
+    "Generate all possible tours of the cities and chose the shortest tour."
+    return shortest_tour(alltours(cities))
+
+def shortest_tour(tours): return min(tours, key=distance_tour)
+
+
+def visualize_tour(tour, style='bo-'):
+    if len(tour) > 1000: plt.figure(figsize=(15,10))
+    start = tour[0:1]
+    visualize_segment(tour + start, style)
+    visualize_segment(start, 'rD')
+
+def visualize_segment(segment, style='bo-'):
+    plt.plot([X(c) for c in segment], [Y(c) for c in segment], style, clip_on=False)
+    plt.axis('scaled')
+    plt.axis('off')
+
+def X(city): "X axis"; return city.real
+
+def Y(city): "Y axis"; return city.imag
+
+
+def tsp(algorithm,cities):
+    t0 = perf_counter()
+    tour = algorithm(cities)
+    t1 = perf_counter()
+    assert Counter(tour) == Counter(cities)
+    visualize_tour(tour)
+    print("{}: {} cities -> tour length {:.0f} (in {:.3} sec)".format(name(algorithm), len(tour), distance_tour(tour),t1-t0))
+
+def name(algorithm): return algorithm.__name__.replace('_tsp', '')
+
+tsp(brute_force,generate_cities(5))
+plt.show()
