78f7e7db7ab2af02ae742e12c15311bc9f6e1ee0
bill <crupib@gmail.com>
2021-09-13 11:08:04 -0400

    Word2

--- Word2.py
+++ Word2.py
@@ -8,6 +8,16 @@ class Word():
     def __str__(self):
         return self.text
     def __repr__(self):
-        return 'Word(self.text)'
-    def __(self,word2):
-        return self.text + word2.text
+        return 'Word("'  + self.text +  '")'
+
+def main():
+# nope
+    first = Word('ha')
+    second = Word('HA')
+    third  = Word('eh')
+    print(first == second)
+    print(second == third)
+    print(first+second)
+    print(first)
+if __name__ == "__main__":
+	main()
