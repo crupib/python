f3cb57094c0c99e01d50ba17a5d329755c3071b1
crupib <crupib@gmail.com>
2021-11-30 22:18:30 -0500

    medium daily digest

---
+++ fak_tst.py
@@ -0,0 +1,5 @@
+from faker import Faker
+fake = Faker()
+
+print(fake.profile())
+print(fake.credit_card_number())
---
+++ fol_tst.py
@@ -0,0 +1,4 @@
+import folium
+
+map = folium.Map(location=[52.8067, -2.1207], zoom_start=14)
+map.save('osm.html')                                                                         
