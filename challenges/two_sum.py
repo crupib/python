bcba43e67da08f67bd179ff72e084980a27a57e2
bill <crupib@gmail.com>
2022-05-27 13:52:48 -0400

    leet code challenges

---
+++ challenges/two_sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def twoSum(self, nums,target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        retlist = []
+        for index, value in enumerate(nums):
+          for jndex, jvalue in enumerate(nums):
+             sum = value+jvalue
+             if sum == target:
+                retlist.append(index)
+        return retlist
+def main():
+       nums = [2,7,11,15]
+       two_sum = Solution()
+       target = 50
+       print(two_sum.twoSum(nums,target))
+if __name__ == "__main__":
+    main()
