8a4684242b825711e2a39ef56c26c71a4f1a65df
bill <crupib@gmail.com>
2022-05-31 14:20:33 -0400

    leetcode challenge add2nums medium

--- challenges/add2nums.py
+++ challenges/add2nums.py
@@ -1,70 +1,84 @@
-# Definition for singly-linked list.
-# Node class
+# A single node of a singly linked list
 class Node:
+  # constructor
+  def __init__(self, data = None, next=None): 
+    self.data = data
+    self.next = next
 
-# Function to initialize the node object
-   def __init__(self, data):
-     self.data = data # Assign data
-     self.next = None # Initialize
-# Linked List class
-# A simple Python program to introduce a linked list
-# Linked List class contains a Node object
+# A Linked List class with a single head node
 class LinkedList:
-# Function to initialize head
-   def __init__(self):
-     self.head = None
-   def printList(self):
-        temp = self.head
-        while (temp):
-            print (temp.data)
-            temp = temp.next
+  def __init__(self):  
+    self.head = None
+  # insertion method for the linked list
+  def insert(self, data):
+    newNode = Node(data)
+    if(self.head):
+      current = self.head
+      while(current.next):
+        current = current.next
+      current.next = newNode
+    else:
+      self.head = newNode
+  # print method for the linked list
+  def printLL(self):
+    current = self.head
+    while(current):
+      print(current.data, end=""),
+      current = current.next
+    print("\n")
+
 class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        temp = l1.head
+  def addTwoNumbers(self, l1, l2):
+        temp1 = l1.head
         temp2 = l2.head
+        sum = 0
         carry = 0
         rlist = LinkedList()
-        node = Node(0) 
-        while(temp):
+        while(temp1):
+          sum = temp1.data+temp2.data
           if carry == 1:
-             node.data = (temp.data+temp2.data+1)
-             carry = 0
-          elif (temp.data+temp2.data) > 9:
-            node.data = 0
-            carry = 1
+            sum = sum+1
+          if sum >= 10:
+             carry = 1
+             sum = sum - 10
+             rlist.insert(sum) 
           else:
-            node.data = (temp.data+temp2.data)
-          node.next = nodenext
-          temp = temp.next
+             carry = 0
+             rlist.insert(sum)
+          temp1 = temp1.next
           temp2 = temp2.next
-        return rlist 
+        if carry == 1:
+           rlist.insert(sum+1)
+        return rlist
+
+# Singly Linked List with insertion and print methods
 def main():
-  l1 = [2,4,3]
-  l2 = [5,6,4]
+#  l1 = [9,9,9,9,9,9,9]
+#  l2 = [9,9,9,9]
+#  l1 = [2,4,3]
+#  l2 = [5,6,4]
+  l1 = [0]
+  l2 = [0]
   llist1 = LinkedList()
   llist2 = LinkedList()
   sol = Solution()
   temp1=[]
   temp2=[]
+
   for x in reversed(l1):
       temp1.append(Node(x))
   for y in reversed(l2):
       temp2.append(Node(y))
-  llist1.head = temp1[0]
-  llist1.head.next=temp1[1]
-  for item in temp1[2:len(temp1)]:
-      llist1.head.next.next=item
-#  llist1.printList()
-  llist2.head = temp2[0]
-  llist2.head.next=temp2[1]
-  for item in temp2[2:len(temp2)]:
-      llist2.head.next.next=item
-#  llist2.printList()
+  
+  numzeros = len(l1)-len(l2)
+  for item in temp1:
+     llist1.insert(item.data) 
+  for item in temp2:
+     llist2.insert(item.data) 
+  if numzeros > 0:
+     for i in range(numzeros):
+         llist2.insert(0)   
   rlist=sol.addTwoNumbers(llist1,llist2)
+  rlist.printLL()
 if __name__ == "__main__":
     main()
