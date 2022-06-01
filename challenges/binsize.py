e7a6425c7d501ac4b0138b3085707b57c5fd79a6
bill <crupib@gmail.com>
2022-06-01 14:14:00 -0400

    Leetcode challenge - binstr

--- challenges/binsize.py
+++ challenges/binsize.py
@@ -1,39 +1,32 @@
 import string
 class Solution(object):
-   def hasAllCodes(self,s,k):
-     counter = 0
-     if k == 2:
-       found0 = s.find('00') 
-       if found0 == -1
-         found = False
-       found1 = s.find('01')
-       if found1 == -1
-         found = False
-       found2 = s.find('10')
-       if found2 == -1
-         found = False
-       found3 = s.find('11')
-       if found3 == -1
-         found = False
-        
-       return found 
-     counter = 0
-     if k == 1:
-       if s.find('0') != -1 :
-          counter += 1
-          found = True
-       elif (s.find('1')) != -1:
-          counter += 1
-          found = True
-       else:
-          found = False
-       if counter < 4:
-          found = False
-       return found 
-           
+   def binstr(self,k,k2sq):
+     getBinary = lambda x, n: format(x, 'b').zfill(n)
+     binaryList = []
+     for i in range(k2sq):
+       binaryList.append(getBinary(i,k))
+     return binaryList 
+   def hasAllCodes(self,s,k,k2sq):
+       found = 0
+       binList = self.binstr(k,k2sq)
+       for item in binList:
+          found = s.find(item)
+          if found == -1:
+            return False
+       return True    
 def main():
-    s = "001101100"
     sol = Solution()
-    print(sol.hasAllCodes(s,2))
+    k = 2
+    s = "00110110"
+    k2sq = 2**k
+    print(sol.hasAllCodes(s,k,k2sq))
+    s = "0110"
+    k = 1
+    k2sq = 2**k
+    print(sol.hasAllCodes(s,k,k2sq))
+    s = "0110"
+    k = 2
+    k2sq = 2**k
+    print(sol.hasAllCodes(s,k,k2sq))
 if __name__ == "__main__":
     main()
