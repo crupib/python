6534b3e1a89d717a833a34f18f311026b41138fd
bill <crupib@gmail.com>
2022-06-02 13:58:09 -0400

    Leetcode Roman Numerals (easy)

--- challenges/binsize.py
+++ challenges/binsize.py
@@ -1,14 +1,15 @@
 import string
 class Solution(object):
+   k2sq = 0
    def binstr(self,k,k2sq):
      getBinary = lambda x, n: format(x, 'b').zfill(n)
      binaryList = []
      for i in range(k2sq):
        binaryList.append(getBinary(i,k))
      return binaryList 
-   def hasAllCodes(self,s,k,k2sq):
+   def hasAllCodes(self,s,k):
        found = 0
-       binList = self.binstr(k,k2sq)
+       binList = self.binstr(k,self.k2sq)
        for item in binList:
           found = s.find(item)
           if found == -1:
@@ -18,15 +19,22 @@ def main():
     sol = Solution()
     k = 2
     s = "00110110"
-    k2sq = 2**k
-    print(sol.hasAllCodes(s,k,k2sq))
+    sol.k2sq = 2**k
+    print(sol.hasAllCodes(s,k))
     s = "0110"
     k = 1
-    k2sq = 2**k
-    print(sol.hasAllCodes(s,k,k2sq))
+    sol.k2sq = 2**k
+    print(sol.hasAllCodes(s,k))
     s = "0110"
     k = 2
-    k2sq = 2**k
-    print(sol.hasAllCodes(s,k,k2sq))
+    sol.k2sq = 2**k
+    print(sol.hasAllCodes(s,k))
+    s = "000001010100101110011111"
+    k = 3
+    print(sol.hasAllCodes(s,k))
+    s = "00011111"
+    k = 3
+    print(sol.hasAllCodes(s,k))
+
 if __name__ == "__main__":
     main()
---
+++ challenges/roman.py
@@ -0,0 +1,30 @@
+import string
+class Solution(object):
+   
+   def romanToInt(self,s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+        i = 0
+        num = 0
+        while i < len(s):
+           if i+1<len(s) and s[i:i+2] in roman:
+              print(s[i:i+2])
+              num+=roman[s[i:i+2]]
+              i+=2
+           else:
+              num+=roman[s[i]]
+              i+=1
+        return num
+def main():
+    sol = Solution()
+    s = "MCMXCIV"
+    print(sol.romanToInt(s))
+    s = "LVIII"
+    print(sol.romanToInt(s))
+    s = "III"
+    print(sol.romanToInt(s))
+if __name__ == "__main__":
+    main()
