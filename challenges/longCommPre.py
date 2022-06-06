eb182ac353b48fe44784b2862a9e88821fcc7cfc
bill <crupib@gmail.com>
2022-06-06 10:45:30 -0400

    Long Common prefix

--- challenges/longCommPre.py
+++ challenges/longCommPre.py
@@ -1,5 +1,13 @@
 import string
 class Solution(object):
+  def allEqual(self, prefixtemp):
+        ele = prefixtemp[0]
+        chk = True
+        for item in prefixtemp:
+          if ele != item:
+              chk = False
+              break
+        return chk
   def longestCommonPrefix(self, strs):
       """
       :type strs: List[str]
@@ -8,19 +16,16 @@ class Solution(object):
       prefixtemp = []
       precount = 0
       compre = []
-      while precount < len(strs):
+      numberOfStrings = len(strs)
+      while precount <= numberOfStrings:
         for item in strs:
            prefixtemp.append(item[precount])
-        ele = prefixtemp[0]
-        chk = False
-        for item in prefixtemp:
-          if ele != item:
-              chk = False
-              break;
-        if (chk == False) :
-           prefixtemp=[]         
-        else:
+        allEqual = self.allEqual(prefixtemp)
+        if allEqual == True: 
            compre.append(prefixtemp[0])
+           prefixtemp = []
+        else:
+           prefixtemp = []
         precount += 1
       return compre
 def main():
