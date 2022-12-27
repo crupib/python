4a39621ba75028bd1dda56eda228e5ac5767b3c2
crupib <crupib@gmail.com>
2022-12-27 10:16:37 -0500

    python games stuff

--- games/hello.py
+++ games/hello.py
@@ -3,3 +3,7 @@ print('Hello World!')
 print('What is your name?')
 myName = input()
 print('It is good to meet you, ' + myName)
+number = 12
+myName = "Lord Templeton"
+print("Hello " +myName+ " your number is " + str(number))
+
