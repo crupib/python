4365a510b2bead4fa25c2fc2af752a65a9ab0a2c
crupib <crupib@gmail.com>
2024-05-12 13:24:41 -0400

    With out openai key

--- chatgpt/test_gpt_api.py
+++ chatgpt/test_gpt_api.py
@@ -1,4 +1,5 @@
 import openai
+import os
 def test_chatgpt(prompt):
    response = openai.Completion.create(
      engine="gpt-3.5-turbo-instruct",
@@ -11,6 +12,10 @@ def test_chatgpt(prompt):
    message = response.choices[0].text.strip()
    return message
 
+value = os.getenv('OPENAI_API_KEY')
+if value is None:
+    print("The environment variable does not exist or has no value.")
+openai.api_key  = value
 prompt = "What is the captial of Italy?"
 response = test_chatgpt(prompt)
 print(response)
