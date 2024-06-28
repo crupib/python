aeb06c474328d99ea4ece0e680ea38a51bf0e238
Bill <crupib@gmail.com>
2024-06-27 20:44:23 -0400

    basic

---
+++ python_basics.py
@@ -0,0 +1,14 @@
+x = -100
+fruits = ["apple","banana","cherry"]
+for fruit in fruits:
+    print(fruit)
+if x > 0:
+    print("x is positive")
+else:
+    print("x is negative")
+numbers = [1,2,3,4,5]
+for number in numbers:
+    print(number)
+numbers = list(range(5))
+print(numbers)
+
