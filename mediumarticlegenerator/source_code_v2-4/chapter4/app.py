4afb52ddfb9e03c229422c64265a3c534a480f78
William Crupi <crupib@gmail.com>
2024-11-20 21:44:42 -0500

    get rid of apikey

--- mediumarticlegenerator/source_code_v2-4/chapter4/app.py
+++
@@ -1,9 +0,0 @@
-import os
-from apikey import apikey
-import streamlit as st
-
-os.environ["OPENAI_API_KEY"] = apikey
-
-st.title('Medium Article Generator')
-topic = st.text_input('Input your topic of interest')
-
