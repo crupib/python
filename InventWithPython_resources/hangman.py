68557ffba31ccc73ea36a371837096a2c39081a0
crupib <crupib@gmail.com>
2024-12-21 10:45:10 -0500

    coinflips and hangman

--- InventWithPython_resources/hangman.py
+++ InventWithPython_resources/hangman.py
@@ -1,51 +1,56 @@
 import random
-HANGMAN_PICS = ['''
+HANGMAN_PICS = [r'''
   +---+
       |
       |
       |
-     ===''', '''
+     ===''', r'''
   +---+
   O   |
       |
       |
-     ===''', '''
+     ===''', r'''
   +---+
   O   |
   |   |
       |
-     ===''', '''
+     ===''', r'''
   +---+
   O   |
  /|   |
       |
-     ===''', '''
+     ===''', r'''
   +---+
   O   |
  /|\  |
       |
-     ===''', '''
+     ===''', r'''
   +---+
   O   |
  /|\  |
  /    |
-     ===''', '''
+     ===''', r'''
   +---+
   O   |
  /|\  |
  / \  |
      ===''']
-words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
+
+words = '''
+ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret
+fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon
+python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad
+trout turkey turtle weasel whale wolf wombat zebra
+'''.split()
+
 
 def getRandomWord(wordList):
     # This function returns a random string from the passed list of strings.
     wordIndex = random.randint(0, len(wordList) - 1)
     return wordList[wordIndex]
-
 def displayBoard(missedLetters, correctLetters, secretWord):
     print(HANGMAN_PICS[len(missedLetters)])
     print()
-
     print('Missed letters:', end=' ')
     for letter in missedLetters:
         print(letter, end=' ')
